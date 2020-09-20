84ea3e1c0744679c2f20353a31a4743f57127a36
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-09-20 19:02:22 +0700

    Update settings.py

--- web_Img2Lua_MiniWorld/settings.py
+++ web_Img2Lua_MiniWorld/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 AUTH_USER_MODEL = 'home.MyUser'
@@ -123,4 +122,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-django_heroku.settings(locals())
