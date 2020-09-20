4d2a0a284c5a2f9d14b942d0729cea33f85efd52
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-09-20 19:11:13 +0700

    a

--- web_Img2Lua_MiniWorld/settings.py
+++ web_Img2Lua_MiniWorld/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+ALLOWED_HOSTS = ['luascript.herokuapp.com']
 ROOT_URLCONF = 'web_Img2Lua_MiniWorld.urls'
 
 TEMPLATES = [
